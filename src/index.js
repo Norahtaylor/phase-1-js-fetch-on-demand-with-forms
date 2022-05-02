6675cfbb271ecc434cade07127bdde5471369bca
Norah Taylor <emt368@gmail.com>
2022-05-02 19:46:47 -0400

    commit tree b7e484c426f88070d3ba7371f0afc513fdad8521

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  let inputForm = document.querySelector('form')
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const input = document.getElementById('searchByID')
+    //console.log(e.target.searchByID.value)
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(json => {
+        const title = document.querySelector('section#movieDetails h4')
+        const summary = document.querySelector('section#movieDetails p')
+        title.textContent = json.title 
+        summary.textContent = json.summary 
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
